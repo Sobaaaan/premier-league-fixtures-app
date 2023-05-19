c545c0f01537067b086245fa33bc0a03ee8d9266
Sobaaaan <mohammadsoban74@gmail.com>
2023-05-19 22:35:53 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import FixturesList from "./FixturesList";
 
-function App() {
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: #f2f2f2;
+`;
+
+const AppHeader = styled.h1`
+  text-align: center;
+  margin-bottom: 40px;
+  color: #333;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <AppHeader>Premier League Fixtures App</AppHeader>
+      <FixturesList />
+    </AppContainer>
   );
-}
+};
 
 export default App;
---
+++ src/FixturesList.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const FixturesContainer = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #f7f7f7;
+  border-radius: 8px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+`;
+
+const FixturesHeader = styled.h2`
+  text-align: center;
+  margin-bottom: 20px;
+  color: #333;
+`;
+
+const FixtureList = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const FixtureItem = styled.li`
+  margin-bottom: 8px;
+  padding: 10px;
+  background-color: #fff;
+  border-radius: 4px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+`;
+
+const FixtureTeams = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-weight: bold;
+`;
+
+const FixtureTeamName = styled.span`
+  color: #333;
+`;
+
+const FixturesList = () => {
+    const [fixtures, setFixtures] = useState([]);
+
+    useEffect(() => {
+        fetchFixtures();
+    }, []);
+
+    const fetchFixtures = async () => {
+        const url = "https://api-football-v1.p.rapidapi.com/v3/fixtures?league=39&season=2022";
+        const options = {
+            method: "GET",
+            headers: {
+                "X-RapidAPI-Key": "ee16b11841msha961271197ecb67p118d9cjsn6b7ad224a61a",
+                "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+            },
+        };
+
+        try {
+            const response = await fetch(url, options);
+            const result = await response.json();
+            setFixtures(result.response);
+        } catch (error) {
+            console.error("Error fetching fixtures:", error);
+        }
+    };
+
+    return (
+        <FixturesContainer>
+            <FixturesHeader>Premier League Fixtures</FixturesHeader>
+            <FixtureList>
+                {fixtures.map((fixture) => (
+                    <FixtureItem key={fixture.fixture.id}>
+                        <FixtureTeams>
+                            <FixtureTeamName>{fixture.teams.home.name}</FixtureTeamName>
+                            <span>vs</span>
+                            <FixtureTeamName>{fixture.teams.away.name}</FixtureTeamName>
+                        </FixtureTeams>
+                    </FixtureItem>
+                ))}
+            </FixtureList>
+        </FixturesContainer>
+    );
+};
+
+export default FixturesList;
